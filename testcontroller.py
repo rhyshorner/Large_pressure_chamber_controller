3731f5fefd9609cefd0c0acae43c7a4d5cf649ae
Rhys Horner <rhyshorner@gmail.com>
2019-02-05 17:31:20 +1100

    v

--- testcontroller.py
+++ testcontroller.py
@@ -9,16 +9,16 @@ pfd = pifacedigitalio.PiFaceDigital() # creates a PiFace Digtal object
 #--------------------------------------------------------------
 try:
     while True:
-    pfd.output_pins[1].turn_on()    # turn on/set high the second LED
-    pfd.output_pins[2].set_high()   # turn on/set high the third LED
-    pfd.relays[0].value = 1  # turn on/set high the first relay
-    pfd.input_pins[1].value
-    sleep(1)
-    pfd.output_pins[1].turn_off()    # turn on/set high the second LED
-    pfd.output_pins[2].set_low()   # turn on/set high the third LED
-    pfd.relays[0].value = 0  # turn on/set high the first relay
-    pfd.input_pins[1].value
-    sleep(1)
+        pfd.output_pins[1].turn_on()    # turn on/set high the second LED
+        pfd.output_pins[2].set_high()   # turn on/set high the third LED
+        pfd.relays[0].value = 1  # turn on/set high the first relay
+        pfd.input_pins[1].value
+        sleep(1)
+        pfd.output_pins[1].turn_off()    # turn on/set high the second LED
+        pfd.output_pins[2].set_low()   # turn on/set high the third LED
+        pfd.relays[0].value = 0  # turn on/set high the first relay
+        pfd.input_pins[1].value
+        sleep(1)
 except:
     pfd.relays[0].value = 0  # turn on/set high the first relay
 

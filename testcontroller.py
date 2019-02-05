dda04ad627579b090f7fb7b8bdfc32649c98e1dd
Rhys Horner <rhyshorner@gmail.com>
2019-02-05 17:32:18 +1100

    dfs

--- testcontroller.py
+++ testcontroller.py
@@ -20,6 +20,8 @@ try:
         pfd.input_pins[1].value
         sleep(1)
 except:
+    pfd.output_pins[1].turn_off()    # turn on/set high the second LED
+    pfd.output_pins[2].set_low()   # turn on/set high the third LED
     pfd.relays[0].value = 0  # turn on/set high the first relay
 
 

422e4e8f7319d2227a364844ec0893dbe46fa9b2
Rhys Horner <rhyshorner@gmail.com>
2019-02-06 11:16:49 +1100

    dfg

--- testcontroller.py
+++ testcontroller.py
@@ -13,13 +13,13 @@ while True:
     pfd.output_pins[2].set_high()   # turn on/set high the third LED
 
     pfd.relays[0].value = pfd.input_pins[1].value  # turn on/set high the first relay
-    print("input pin 1 is: " + pfd.input_pins[1].value)
+    print("input pin 1 is: " + str(pfd.input_pins[1].value))
     sleep(1)
     pfd.output_pins[1].turn_off()    # turn on/set high the second LED
     pfd.output_pins[2].set_low()   # turn on/set high the third LED
 
     pfd.relays[0].value = pfd.input_pins[1].value  # turn on/set high the first relay
-    print("input pin 1 is: " + pfd.input_pins[1].value)
+    print("input pin 1 is: " + str(pfd.input_pins[1].value))
     sleep(1)
 #except:
  #   pfd.output_pins[1].turn_off()    # turn on/set high the second LED

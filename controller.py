0fcdb30849d021fe06863955e3144d3b0bb90cfb
Rhys Horner <rhyshorner@gmail.com>
2019-02-22 10:28:06 +1100

    d

--- controller.py
+++ controller.py
@@ -78,6 +78,7 @@ while True:
     debounce_over_p_auto_starttimerr = round(time.time(), 1)
     thecurrenttimer = round(time.time() - debounce_over_p_auto_starttimerr, 1)
     print("current time: " + str(thecurrenttimer))
+    sleep(2)
     if over_p_auto_sw == 1 and over_p_auto_sw_toggle == 0 and debounce_over_p_auto_flag == 0:
         if debounce_over_p_auto_flag == 0:
             debounce_over_p_auto_flag = 1
